857140851aaed92bc6deb23f9e46ca9f2a8d349c
aprilcampos <aprilcamille.campos@gmail.com>
2025-07-24 13:06:25 +0800

    add initialization and frequencies

--- rent_scheduler.rb
+++ rent_scheduler.rb
@@ -1,5 +1,15 @@
 require 'date'
 
 class RentScheduler
+  FREQUENCIES = {
+    'weekly' => 7,
+    'fortnightly' => 14,
+    'monthly' => 30
+  }
+
+  def initialize(rent)
+    @rent = rent
+  end
+
 end
 
