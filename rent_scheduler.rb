73865ef14bd2a057ef6d256796ad033af0d466f5
aprilcampos <aprilcamille.campos@gmail.com>
2025-07-24 13:47:33 +0800

    convert start and end dates from string to date

--- rent_scheduler.rb
+++ rent_scheduler.rb
@@ -10,6 +10,8 @@ class RentScheduler
   def initialize(rent)
     @rent_amount = rent[:rent_amount]
     @rent_frequency = rent[:rent_frequency]
+    @rent_start_date = Date.parse(rent[:rent_start_date])
+    @rent_end_date = Date.parse(rent[:rent_end_date])
   end
 
 
